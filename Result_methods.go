05155d27a441bf61421cf481c8105c206a82e4c5
Ben <i@auburn.edu>
2018-06-23 15:58:19 -0600

    More methods and gofmt

--- Result_methods.go
+++ Result_methods.go
@@ -1,16 +1,14 @@
 package main
 
-import (
-
-)
+import ()
 
 // Methods that parse results of a specific command and returns true iff we
 // obtain an expected result. All code used should stay within this method even
 // if that means re-writing the same code
 
 func get_completed_status() bool {
-  if true != false {
-    return true
-  }
-  return false
+	if true != false {
+		return true
+	}
+	return false
 }
--- Runner_runner.go
+++ Runner_runner.go
@@ -1,41 +1,66 @@
 package main
 
 import (
-  "bytes"
-  "fmt"
-  "log"
-  "os/exec"
-  "strings"
-  "time"
-  "gopkg.in/gomail.v2"
+	"bytes"
+  "bufio"
+	"fmt"
+	"log"
+  "os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-  output := run_command("ls -lah")
-  // delay(1)
-  fmt.Println(output)
+  text_arr := read_commands_from_file("commands_sim/commands1.in")
+  fmt.Println(strings.Join(text_arr, "\n"))
+
+	// output := run_command("ls -lah")
+	// delay(1)
+	// fmt.Println(output)
 }
 
-func read_command_from_file() {
+func executer(command string) {
+  
+}
 
+func read_commands_from_file(filepath string) []string {
+  file, err := os.Open(filepath)
+  c(err)
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    if len(scanner.Text()) > 0 {
+      lines = append(lines, scanner.Text())
+    }
+
+  }
+  c(scanner.Err())
+  return lines
 }
 
 func run_command(command string) string {
-  commands_split := strings.Split(command, " ")
-  cmd := exec.Command(commands_split[0], commands_split[1:]...)
-  var out bytes.Buffer
-  cmd.Stdout = &out
-  err := cmd.Run()
-  c(err)
-  return strings.TrimSpace(out.String())
+	commands_split := strings.Split(command, " ")
+	cmd := exec.Command(commands_split[0], commands_split[1:]...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	c(err)
+	return strings.TrimSpace(out.String())
 }
 
 func delay(minutes int) {
-  time.Sleep(time.Duration(minutes) * time.Minute)
+	time.Sleep(time.Duration(minutes) * time.Minute)
 }
 
 func c(err error) {
-  if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func alert() {
+	send_text_alert()
 }
